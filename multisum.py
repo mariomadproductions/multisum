327e9609ac0264783f0efdca707cb3634300b4f6
mariomadproductions <mariomadproductions@github.com>
2021-02-17 20:51:17 +0000

    Changed command syntax, updated help, added (basic) GameHeader format support

--- multisum.py
+++ multisum.py
@@ -2,15 +2,19 @@
 """multisum.
 
 Usage:
-multisum.py [--plain | --xml] [--one-game] [--mktxt] FILE ...
+multisum.py --format=<format> [--one-game] [--mktxt] <file> ...
 
 Options:
-  -h, --help        Show this screen.
-  -v, --version     Show version.
-  -x, --xml         Output in xml datfile format.
-  -p, --plain       Output in plain format.
-  -o, --one-game    If format is xml,
-  -t, --mktxt       Create ".multisum.txt" files for each hashed file.
+  -h, --help               Show this screen.
+  -v, --version            Show version.
+  -f, --format=<format>    Format to output in.
+                           Values:
+                           gh - GameHeader
+                           xml - xml datfile
+                           plain - plain text
+  -o, --one-game           If format is xml, and multiple files are selected,
+                           put everything under one "game" tag.
+  -t, --mktxt              Create ".multisum.txt" files for each file.
 
 """
 from docopt import docopt
@@ -18,8 +22,36 @@ import rhash
 from pathlib import Path
 
 format_strings={
+'gh': \
+[
+'',
+'----| File Data |--------------------------------------------------\n'
+'System:             Unknown File\n'
+'Archive:            \n'
+'File:               {filename}\n'
+'Size (Bytes):       {size}\n'
+'CRC32:              {crc32}\n'
+'MD5:                {md5}\n'
+'SHA1:               {sha1}\n'
+'SHA256:             {sha256}\n'
+'-------------------------------------------------------------------\n'
+'\n',
+'',
+''
+],
+
+'xml': \
+[
+'<game name="{filename}">\n',
+'<rom name="{filename}" size="{size}" crc="{crc32}" md5="{md5}" '
+'sha1="{sha1}" sha256="{sha256}"/>\n',
+'</game>\n',
+'\n'
+],
+
 'plain' : \
-['',
+[
+'',
 'filename : {filename}\n'
 'size     : {size}\n'
 'crc32    : {crc32}\n'
@@ -27,18 +59,8 @@ format_strings={
 'sha1     : {sha1}\n'
 'sha256   : {sha256}\n',
 '\n',
-
-''],
-
-'xml': \
-['<game name="{filename}">\n',
-
-'<rom name="{filename}" size="{size}" crc="{crc32}" md5="{md5}" '
-'sha1="{sha1}" sha256="{sha256}"/>\n',
- 
-'</game>\n',
-
-'\n']
+''
+],
 }
 
 def print_format_string(filename,size,hasher,output_format,part):
@@ -53,11 +75,8 @@ def print_format_string(filename,size,hasher,output_format,part):
 
 def main(arguments):
     hasher = rhash.RHash(rhash.CRC32 | rhash.MD5 | rhash.SHA1 | rhash.SHA256)
-    if arguments['--xml']:
-        output_format = 'xml'
-    else:
-        output_format = 'plain'
-    for file in arguments['FILE']:
+    output_format = arguments['--format']
+    for file in arguments['<file>']:
         filename = Path(file).name
         size = Path(file).stat().st_size
         hasher.update_file(file)
