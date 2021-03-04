e5728b537636109615d1cb93ec57d7557822844f
mariomadproductions <mariomadproductions@github.com>
2021-03-04 16:04:01 +0000

    Added recursive and 'current directory' modes

--- multisum.py
+++ multisum.py
@@ -2,7 +2,7 @@
 """multisum.
 
 Usage:
-multisum.py --format=<format> [--one-game] [--mktxt] <file> ...
+multisum.py --format=<format> [--recursive] [--one-game] [--mktxt] <path> ...
 
 Options:
   -h, --help               Show this screen.
@@ -12,6 +12,7 @@ Options:
                            gh - GameHeader
                            xml - xml datfile
                            plain - plain text
+  -r, --recursive          Hash files recursively.
   -o, --one-game           If format is xml, and multiple files are selected,
                            put everything under one "game" tag.
   -t, --mktxt              Create ".multisum.txt" files for each file.
@@ -19,7 +20,7 @@ Options:
 """
 from docopt import docopt
 from zlib import crc32
-from hashlib import sha1, sha256, md5
+from hashlib import md5, sha1, sha256
 from pathlib import Path
 
 format_strings={
@@ -64,8 +65,11 @@ format_strings={
 ],
 }
 
+arguments = docopt(__doc__, version='multisum.py')
+output_format = arguments['--format']
+input_path = arguments['<path>']
+recursive_mode = arguments['--recursive']
 
-#This function is based on one by Icyelut
 def hash_file(file_path):
     calculated_crc32 = 0
     calculated_md5 = md5()
@@ -88,27 +92,51 @@ def hash_file(file_path):
     
     return output_crc32, output_md5, output_sha1, output_sha256
 
-def print_format_string(filename,size,hashes,output_format,part):
-    print(format_strings[output_format][part].format(
-            filename=filename,
-            size=size,
-            crc32=hashes[0],
-            md5=hashes[1],
-            sha1=hashes[2],
-            sha256=hashes[3]
-            ),end='')
+def print_format_string(filename,size,hashes,part):
+    if part == 3:
+        print(format_strings[output_format][part],end='')
+    else:
+        print(format_strings[output_format][part].format(
+                filename=filename,
+                size=size,
+                crc32=hashes[0],
+                md5=hashes[1],
+                sha1=hashes[2],
+                sha256=hashes[3]
+                ),end='')
 
-def main(arguments):
-    output_format = arguments['--format']
-    for file in arguments['<file>']:
-        filename = Path(file).name
-        size = Path(file).stat().st_size
-        hashes = hash_file(file)
-        print_format_string(filename,size,hashes,output_format,0)
-        print_format_string(filename,size,hashes,output_format,1)
-        print_format_string(filename,size,hashes,output_format,2)
-    print_format_string(filename,size,hashes,output_format,3)
+def process_file(path):
+    if recursive_mode:
+        filename = Path(path)
+    else:
+        filename = Path(path).name
+    size = Path(path).stat().st_size
+    hashes = hash_file(path)
+    print_format_string(filename,size,hashes,0)
+    print_format_string(filename,size,hashes,1)
+    print_format_string(filename,size,hashes,2)
+
+def main():
+    path_not_dir_found = False
+    for path_string in input_path:
+        if Path(path_string).is_dir():
+            if recursive_mode:
+                for glob_path in Path(path_string).glob('**/*'):
+                    try:
+                        process_file(glob_path)
+                    except IsADirectoryError:
+                        pass
+            else:
+                for iterdir_path in Path(path_string).iterdir():
+                    try:
+                        process_file(iterdir_path)
+                    except IsADirectoryError:
+                        pass
+        else:
+            path_not_dir_found = True
+            process_file(path_string)
+    if path_not_dir_found:
+        print_format_string(None,None,None,3)
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='multisum.py')
-    main(arguments)
+    main()
